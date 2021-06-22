8ff3b8aa785edf1c660f987e0a7f0c5f1868db14
jideabdqudus <jideabdqudus@gmail.com>
2021-06-22 11:31:22 +0100

    +

---
+++ src/components/home/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <Container>
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Row, Col, Container } from "reactstrap";
 
 import HeaderContainer from "../layout/index";
 import Aside from "../components/aside/index";
+import Home from "../components/home/index";
 
 const Dashboard = () => {
   return (
@@ -12,7 +13,9 @@ const Dashboard = () => {
           <Col lg={3}>
             <Aside />
           </Col>
-          <Col lg={6}>Loremx30</Col>
+          <Col lg={6}>
+            <Home />
+          </Col>
         </Row>
       </Container>
     </div>
