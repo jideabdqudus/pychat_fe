03f694cbb6a7d0b303485b5a8f35b1099d268347
jideabdqudus <jideabdqudus@gmail.com>
2021-09-26 01:11:05 +0100

    Birthday Portion

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Container, Button, Row, Col } from "reactstrap";
+
 
 const Info = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <div className="info">
+        <div className="info__birthdays">
+          <h6>
+            <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yc/r/uZcEIx7yRqF.png?_nc_eui2=AeFDDrqCjlGxkWk5PbCG_kue_63W3ZSARWb_rdbdlIBFZiDXh31Z2sj6M0EM3f72veD_kBahwkh-UchsCGnDPPaT" alt="present" width="20" />
+            Birthdays
+          </h6>
+          <ul>
+            <li>
+              <b>Osaretin</b>'s Birthday is today
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Container>
   )
 }
 
